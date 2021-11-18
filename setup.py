d0d0232ed381735386a1d97cee5b5525c5376063
Iain Bailey <iainbailey@gmail.com>
2021-11-18 21:45:18 +0000

    added .idea to gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cattbl',
+    version='0.0.0',
+    packages=['cattbl'],
+    url='',
+    license='',
+    author='Iain Bailey',
+    author_email='iainbailey@gmail.com',
+    description='Working with various loss tables used for CAT modelling'
+)

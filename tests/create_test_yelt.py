46986962c2933804b1c9ca077468169a57770d47
Iain Bailey <iainbailey@gmail.com>
2022-04-16 14:10:26 +0100

    Added a new test YLT with loss allocated to regions and loss sources. Changed the random number engine used in line with Numpy updates in the script

--- tests/create_test_yelt.py
+++ tests/create_test_yelt.py
@@ -47,9 +47,9 @@ def main():
     min_loss = 1e3
     ofilename = "_data/example_pareto_poisson_yelt.csv"
 
-    rs = np.random.RandomState(seed=seed)
+    rng = np.random.default_rng(seed=seed)
 
-    yelt = poisson_pareto_yelt(rs,
+    yelt = poisson_pareto_yelt(rng,
                                freq0=0.5,  # Number of events per year
                                n_years=n_years,
                                min_loss=min_loss,
@@ -61,7 +61,7 @@ def main():
 
     # Create a second YELT with two models and two losses
     ofilename2 = "_data/example_two_models_grossnet.csv"
-    yelt2 = poisson_pareto_yelt(rs,
+    yelt2 = poisson_pareto_yelt(rng,
                                freq0=1.0,  # Number of events per year
                                n_years=n_years,
                                min_loss=min_loss,
@@ -84,6 +84,51 @@ def main():
     # Write to file
     yelt_combined.to_csv(ofilename2, index=True)
     print(f"Written to {ofilename2}")
+
+    # Create a Year Event Allocated Loss Table where event loss has been split
+    print(f"Creating an allocated YELT among regions and loss sources")
+    ofilename3 = "_data/example_allocated_loss.csv"
+    yealt = yelt_combined[['GrossLoss']].copy()
+    print(f"Total Loss: {yealt['GrossLoss'].sum():,.0f}")
+
+    # Random split between three different regions
+    r = rng.random(size=(len(yealt), 2))
+    r.sort(axis=1)
+    region_splits = np.hstack([r[:, [0]], np.diff(r, axis=1), 1 - r[:, [1]]])
+
+    # Apply percentage split to each loss
+    for i in range(region_splits.shape[1]):
+        yealt[i + 1] = yealt['GrossLoss'] * region_splits[:, i]
+
+    # Collapse into single loss row
+    yealt.columns.rename('RegionID', inplace=True)
+    yealt = yealt.drop('GrossLoss', axis=1).stack().rename('Loss').to_frame()
+    print(f"Initially {len(yealt):,.0f} rows for regions")
+    print(f"Total Loss: {yealt['Loss'].sum():,.0f}")
+
+    # Replace some region IDs, so we don't have the same 3 regions for each
+    # loss.
+    n_replace = 10000
+    idx_replace = rng.choice(range(len(yealt)), replace=False, size=n_replace)
+    new_regionids = yealt.index.get_level_values('RegionID').values
+    new_regionids[idx_replace] = 4
+
+    index_names = yealt.index.names
+    yealt = yealt.reset_index()
+    yealt['RegionID'] = new_regionids
+    print(f"Total Loss: {yealt['Loss'].sum():,.0f}")
+
+    # Add a source of loss ID randomly to each loss
+    yealt['LossSourceID'] = rng.integers(low=1, high=6, size=len(yealt))
+    yealt = yealt.set_index(index_names + ['LossSourceID'])
+
+    # Get rid of any duplicates
+    yealt = yealt.groupby(yealt.index.names).sum()
+    print(yealt.groupby(['RegionID', 'LossSourceID']).sum().unstack('RegionID'))
+    print(f"Finally {len(yealt):,.0f} rows for regions and loss sources")
+
+    yealt.to_csv(ofilename3, index=True)
+    print(f"Written to {ofilename3}")
 
 
 if __name__ == "__main__":

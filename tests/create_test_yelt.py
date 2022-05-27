476fd969f63735068c97a7c32118eb4926b64f41
Iain Bailey <iainbailey@gmail.com>
2022-05-27 15:19:57 -0700

    Separate main function into smaller parts

--- tests/create_test_yelt.py
+++ tests/create_test_yelt.py
@@ -40,62 +40,46 @@ def poisson_pareto_yelt(random_state, freq0, n_years, min_loss, alpha):
     return yelt
 
 
-def main():
-    """Main script"""
-
-    seed = 12345
-    n_years = 1e5  # Number of years
-    min_loss = 1e3
-    ofilename = "_data/example_pareto_poisson_yelt.csv"
-
-    rng = np.random.default_rng(seed=seed)
-
-    yelt = poisson_pareto_yelt(rng,
-                               freq0=0.5,  # Number of events per year
-                               n_years=n_years,
-                               min_loss=min_loss,
-                               alpha=1.0)
-
-    # Write to file
-    yelt.to_csv(ofilename, index=True)
+def write_file(yelt, ofilename, **kwargs):
+    """Write a series or dataframe to file and print message"""
+    yelt.to_csv(ofilename, **kwargs)
     print(f"Written to {ofilename}")
 
-    # Create a second YELT with two models and two losses
-    ofilename2 = "_data/example_two_models_grossnet.csv"
-    yelt2 = poisson_pareto_yelt(rng,
-                                freq0=1.0,  # Number of events per year
-                                n_years=n_years,
-                                min_loss=min_loss,
-                                alpha=1.5)
 
+def combine_yelts_two_models(yelt1, yelt2):
+    """Combine YELTs from two models into one"""
     # Stack the YELTs together
-    yelt_combined = pd.concat([yelt, yelt2], axis=0, keys=['Model1', 'Model2'],
+    yelt_combined = pd.concat([yelt1, yelt2], axis=0, keys=['Model1', 'Model2'],
                               names=['ModelID'])
 
     # Add a net loss after a limit
     yelt_combined = yelt_combined['Loss'].rename('GrossLoss').to_frame()
-    yelt_combined['NetLoss'] = yelt_combined['GrossLoss']
+
+    return yelt_combined
+
+
+def split_gross_net(yelt, attach, limit):
+    """Create a net and a gross loss by applying layer attachment and limit"""
+    yelt = yelt.rename(columns={'Loss': 'GrossLoss'})
+
+    yelt['NetLoss'] = yelt['GrossLoss']
 
     # Apply deductible
-    yelt_combined['NetLoss'] = (yelt_combined['NetLoss'] - 20e3).clip(lower=0.0)
+    yelt['NetLoss'] = (yelt['NetLoss'] - attach).clip(lower=0.0)
 
     # Apply a limit
-    yelt_combined['NetLoss'] = yelt_combined['NetLoss'].clip(upper=120e3)
+    yelt['NetLoss'] = yelt['NetLoss'].clip(upper=limit)
 
-    # Write to file
-    yelt_combined.to_csv(ofilename2, index=True)
-    print(f"Written to {ofilename2}")
+    return yelt
 
-    # Create a Year Event Allocated Loss Table where event loss has been split
-    print("Creating an allocated YELT among regions and loss sources")
-    ofilename3 = "_data/example_allocated_loss.csv"
-    yealt = yelt_combined[['GrossLoss']].copy()
-    print(f"Total Loss: {yealt['GrossLoss'].sum():,.0f}")
+
+def create_region_splits(yealt, rng):
+    """Split out existing loss in a yelt randomly into different regions"""
 
     # Random split between three different regions
-    r = rng.random(size=(len(yealt), 2))
-    r.sort(axis=1)
-    region_splits = np.hstack([r[:, [0]], np.diff(r, axis=1), 1 - r[:, [1]]])
+    runif = rng.random(size=(len(yealt), 2))
+    runif.sort(axis=1)
+    region_splits = np.hstack([runif[:, [0]], np.diff(runif, axis=1), 1 - runif[:, [1]]])
 
     # Apply percentage split to each loss
     for i in range(region_splits.shape[1]):
@@ -116,20 +100,63 @@ def main():
 
     index_names = yealt.index.names
     yealt = yealt.reset_index()
+
     yealt['RegionID'] = new_regionids
     print(f"Total Loss: {yealt['Loss'].sum():,.0f}")
 
+    return yealt.set_index(index_names)
+
+
+def main():
+    """Main script"""
+
+    seed = 12345
+    n_years = 1e5  # Number of years
+    min_loss = 1e3
+
+    rng = np.random.default_rng(seed=seed)
+
+    yelt = poisson_pareto_yelt(rng,
+                               freq0=0.5,  # Number of events per year
+                               n_years=n_years,
+                               min_loss=min_loss,
+                               alpha=1.0)
+
+    # Write to file
+    write_file(yelt, "_data/example_pareto_poisson_yelt.csv", index=True)
+
+    # Create a second YELT with two models and two losses
+    yelt2 = poisson_pareto_yelt(rng,
+                                freq0=1.0,  # Number of events per year
+                                n_years=n_years,
+                                min_loss=min_loss,
+                                alpha=1.5)
+
+    yelt2 = combine_yelts_two_models(yelt, yelt2)
+
+    # Create Gross and Net columns by applying an attachment and limit
+    yelt2 = split_gross_net(yelt2, 20e3, 120e3)
+
+    # Write to file
+    write_file(yelt2, "_data/example_two_models_grossnet.csv", index=True)
+
+    # Create a Year Event Allocated Loss Table where event loss has been split
+    print("Creating an allocated YELT among regions and loss sources")
+    yealt = yelt2[['GrossLoss']].copy()
+    print(f"Total Loss: {yealt['GrossLoss'].sum():,.0f}")
+
+    yealt = create_region_splits(yealt, rng)
+
     # Add a source of loss ID randomly to each loss
     yealt['LossSourceID'] = rng.integers(low=1, high=6, size=len(yealt))
-    yealt = yealt.set_index(index_names + ['LossSourceID'])
+    yealt = yealt.set_index('LossSourceID', append=True)
 
     # Get rid of any duplicates
     yealt = yealt.groupby(yealt.index.names).sum()
     print(yealt.groupby(['RegionID', 'LossSourceID']).sum().unstack('RegionID'))
     print(f"Finally {len(yealt):,.0f} rows for regions and loss sources")
 
-    yealt.to_csv(ofilename3, index=True)
-    print(f"Written to {ofilename3}")
+    write_file(yealt, "_data/example_allocated_loss.csv", index=True)
 
 
 if __name__ == "__main__":

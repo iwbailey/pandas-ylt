831ffe9037f4f0869274c7f371d394f05020e18a
Iain Bailey <iainbailey@gmail.com>
2024-08-27 21:37:35 +0100

    add all tests for reinstatements

--- tests/test_layer.py
+++ tests/test_layer.py
@@ -9,9 +9,22 @@ from pandas_ylt.layer import Layer
     "limit, agg_limit, reinst_rates, agg_loss, expected",
     [
         # 2 reinstatements at a fixed cost. 1.75 reinstatements used
-        (2.0, 6.0, 0.075, 3.5, 0.2625),
+        (2.0, 6.0, 0.075, 3.5, 1.75 * 2.0 * 0.075),
+
         # 2.5 reinstatements at a fixed cost. 2.25 reinstatements used
-        (2.0, 7.0, 0.075, 4.5, 0.3375),
+        (2.0, 7.0, 0.075, 4.5, 2.25 * 2.0 * 0.075),
+
+        # 2.5 reinstatements at a fixed cost. 2.5 reinstatements used
+        (2.0, 7.0, 0.075, 5.5, 2.5 * 2.0 * 0.075),
+
+        # 2.5 reinstatements with first free. 2.5 reinstatements used
+        (2.0, 7.0, [0.0, 0.075, 0.075], 5.5, 0.0 + 1.5 * 2.0 * 0.075),
+
+        # No reinstatemnts because limit is same as agg limit
+        (2.0, 2.0, 0.0, 5.5, 0.0),
+
+        # No reinstatements where limit is more than agg limit
+        (2.0, 1.5, 0.0, 5.5, 0.0),
     ],
 )
 def test_reinst_cost(limit, agg_limit, reinst_rates, agg_loss, expected):
@@ -20,3 +33,22 @@ def test_reinst_cost(limit, agg_limit, reinst_rates, agg_loss, expected):
     this_lyr = Layer(limit, agg_limit=agg_limit, reinst_rate=reinst_rates)
 
     assert this_lyr.reinst_cost(agg_loss) == approx(expected)
+
+
+
+@pytest.mark.parametrize(
+        "layer_params,",
+        [
+            # Agg limit is 3.5 * limit, but only 2 reinstatements specified
+            ({'limit': 2.0, 'agg_limit': 7.0, 'reinst_rate': [0.0, 0.075]}),
+
+            # Agg limit is 3.5 * limit, but 4 reinstatements specified
+            ({'limit': 2.0, 'agg_limit': 7.0,
+              'reinst_rate': [0.0, 0.075, 0.075, 0.0755]}),
+        ],
+)
+def test_validation_error(layer_params):
+    """Test we get validation errors when using bad parameters"""
+
+    with pytest.raises(ValueError):
+        Layer(**layer_params)
